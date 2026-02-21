7d1fc919d3551e184bdbdab9a595ad506ca9d42e
707 <afanasyeva175@gmail.com>
2026-02-21 15:10:58 +0300

    Create task_2-2_3.py

---
+++ task_2-2_3.py
@@ -0,0 +1,5 @@
+device_name = "спектрофотометр"
+device_number = 1234
+state = "исправен"
+amount = 2
+print(f"Название прибора:\t{device_name}\n Инвентарный номер:\t{device_number}\n Состояние:\t{state}\n Количество:\t{amount}")
